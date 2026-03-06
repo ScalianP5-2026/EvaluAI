22ff11d8b85491b7172980fd5a14d45832ec39e6
IgnacioCastilloFranco <github_portfolio@outlook.es>
2026-03-06 10:34:16 +0100

    changes to deploy in RaspberryPi

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/evaluai/",
   plugins: [react()],
   server: {
     port: 3000,
     host: true,
+    allowedHosts: ["ignacio.software"],
   },
 });
